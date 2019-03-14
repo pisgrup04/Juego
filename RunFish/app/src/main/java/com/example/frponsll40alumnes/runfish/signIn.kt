41352a35eac374dedf8253fca145814aeb72ebc0
OscarFernandezBatalla <oscarfernan@hotmail.es>
2019-03-14 21:03:57 +0100

    implementant nav

---
+++ RunFish/app/src/main/java/com/example/frponsll40alumnes/runfish/signIn.kt
@@ -0,0 +1,12 @@
+package com.example.frponsll40alumnes.runfish
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class signIn : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
