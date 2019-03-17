3c0a9c24e3315a35603d15afeffd0fa709514923
OscarFernandezBatalla <oscarfernan@hotmail.es>
2019-03-17 23:35:04 +0100

    Revert "Sign In fet2"
    This reverts commit 6a447c029628abbb0a81c675c3f191603d696032, reversing
    changes made to e05700dd9458fe641d13ec9d9b38986f12d75283.

--- RunFish/app/src/main/java/com/example/frponsll40alumnes/runfish/MainFragment.kt
+++ RunFish/app/src/main/java/com/example/frponsll40alumnes/runfish/MainFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_main.*
 
 
@@ -25,8 +26,6 @@ class MainFragment : Fragment() {
 
         buttonEnter.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_mainFragment_to_enterFragment))
         buttonSignIn.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_mainFragment_to_signInFragment))
-        button_enter.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_mainFragment_to_enterFragment))
-        button_SignIn.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_mainFragment_to_signInFragment))
     }
 
 
