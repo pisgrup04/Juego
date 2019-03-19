762d8b637d9f367ed4833b372c6502882db70032
bernatosoriogomez <bernatosoriogomez@users.noreply.github.com>
2019-03-19 23:12:22 +0100

    Stats Screen fet

--- RunFish/app/src/main/java/com/example/frponsll40alumnes/runfish/statsFragment.kt
+++ RunFish/app/src/main/java/com/example/frponsll40alumnes/runfish/statsFragment.kt
@@ -1,31 +1,26 @@
 package com.example.frponsll40alumnes.runfish
 
-import android.content.Context
-import android.net.Uri
+
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
-import kotlinx.android.synthetic.main.fragment_sign_in.*
+import kotlinx.android.synthetic.main.fragment_stats.*
 
-
-class SignInFragment : Fragment() {
-
+class statsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+        return inflater.inflate(R.layout.fragment_stats, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        button_comeback.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_signInFragment_to_mainFragment))
-        button_sign_in.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_signInFragment_to_mainFragment))
+        button_comeback.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_statsFragment_to_menuFragment))
     }
-
 }
