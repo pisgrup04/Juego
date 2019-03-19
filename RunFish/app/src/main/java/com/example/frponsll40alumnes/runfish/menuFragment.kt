0b4cd5ab84adcf6199e2f97fbb9a9c56423c2c12
OscarFernandezBatalla <oscarfernan@hotmail.es>
2019-03-19 12:13:51 +0100

    menu_prova

---
+++ RunFish/app/src/main/java/com/example/frponsll40alumnes/runfish/menuFragment.kt
@@ -0,0 +1,28 @@
+package com.example.frponsll40alumnes.runfish
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+
+class menuFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_menu, container, false)
+    }
+
+
+}
