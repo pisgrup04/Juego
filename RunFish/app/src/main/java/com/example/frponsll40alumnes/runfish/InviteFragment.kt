32ddb5c8d58ccaa395d6f794f2989b97d04182f4
bernatosoriogomez <bernatosoriogomez@users.noreply.github.com>
2019-03-20 23:00:49 +0100

    botons invite fragment
    Preguntar per la visiblitat del fragment un cop visistat per primera vegada

--- RunFish/app/src/main/java/com/example/frponsll40alumnes/runfish/InviteFragment.kt
+++ RunFish/app/src/main/java/com/example/frponsll40alumnes/runfish/InviteFragment.kt
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_invite.*
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -27,5 +28,28 @@ class InviteFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_invite, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        /*
+        * TODO: Preguntar per la visiblitat del fragment un cop visistat per primera vegada.
+         */
+        button_friend.setOnClickListener{
+            view.visibility=View.GONE
+        }
+        button_friend1.setOnClickListener{
+            view.visibility=View.GONE
+        }
+        button_friend2.setOnClickListener{
+            view.visibility=View.GONE
+        }
+        button_friend3.setOnClickListener{
+            view.visibility=View.GONE
+        }
+
+        button_cross.setOnClickListener{
+            view.visibility=View.GONE
+        }
+
+    }
 }
